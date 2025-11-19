5b118babd96f6f4ff510cf231f727f0600eb34c9
Nymmh <stickman7415web@gmail.com>
2025-11-19 01:25:41 -0600

    Added footer and save reminder

---
+++ src/renderer/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+    <a href="https://jinxxy.com/Nymh" target="_blank" rel="noopener noreferrer">Nymh</a>
+    <a href="https://discord.gg/rcCCkbDsY3" target="_blank" rel="noopener noreferrer">Discord</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 16px;
+  justify-content: center;
+}
+</style>
