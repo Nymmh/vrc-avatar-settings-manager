53159a6f6e7afb6fd0e6e9b97a584c3a529c18af
Nymh <nymhavis@gmail.com>
2026-03-03 04:03:27 -0600

    Updated Waiting.vue

--- src/renderer/src/components/Waiting.vue
+++ src/renderer/src/components/Waiting.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="waiting">
     <h1>
-      <span class="waiting__header">Waiting for avatar to be loaded...</span>
+      <span class="waiting__header">VRChat is closed or waiting for avatar to be loaded...</span>
       <br />If you are already using the avatar you wish to copy or upload settings to, please swap
       to a different avatar then back.
     </h1>
